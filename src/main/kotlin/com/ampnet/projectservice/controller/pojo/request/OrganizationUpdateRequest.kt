9bf3e460b100139f2f19d292d2c8e551e874b3c0
Eugen Druzin <edruzin@gmail.com>
2020-10-01 10:37:50 +0200

    Remove OrganizationUpdate description size

--- src/main/kotlin/com/ampnet/projectservice/controller/pojo/request/OrganizationUpdateRequest.kt
+++ src/main/kotlin/com/ampnet/projectservice/controller/pojo/request/OrganizationUpdateRequest.kt
@@ -1,8 +1,5 @@
 package com.ampnet.projectservice.controller.pojo.request
 
-import javax.validation.constraints.Size
-
 data class OrganizationUpdateRequest(
-    @field:Size(max = 256)
     val description: String?
 )
