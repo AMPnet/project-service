b6c210e9e860f2c6465727509cc8236be857d895
Eugen Druzin <edruzin@gmail.com>
2020-10-22 18:32:27 +0200

    Squash database migrations

--- src/main/resources/db/migration/V2__index_data.sql
+++ src/main/resources/db/migration/V2__index_data.sql
@@ -1,10 +1,7 @@
-CREATE INDEX idx_document_link ON document(link);
-
 CREATE INDEX idx_project_name ON project(name);
-CREATE INDEX idx_project_organization_uuid ON project(organization_uuid);
-CREATE INDEX idx_project_tag ON project_tag(project_uuid);
-
 CREATE INDEX idx_organization_name ON organization(name);
+
+CREATE INDEX idx_project_organization_uuid_coop ON project(organization_uuid, coop);
 CREATE INDEX idx_org_membership_org_uuid ON organization_membership(organization_uuid);
 CREATE INDEX idx_org_membership_user_uuid ON organization_membership(user_uuid);
-CREATE INDEX idx_org_invitation_email ON organization_invitation(email);
+
--- src/main/resources/db/migration/V4__organization_update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE organization ADD COLUMN header_image VARCHAR;
-ALTER TABLE organization RENAME COLUMN legal_info TO description;
